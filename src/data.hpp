0701cd197dcedadf70ff007d378f9c94a24b7dfd
AKMaily <anna.theimann@outlook.de>
2025-01-15 08:42:39 +0100

    Console output for one device in the correct format

--- src/data.hpp
+++ src/data.hpp
@@ -23,7 +23,7 @@ void printDevices(std::vector<std::shared_ptr<OmniscopeDevice>> &);
 void searchDevices();
 void startMeasurementAndWrite(std::vector<std::string> &, std::string &, bool &);
 void selectDevices();
-void printOrWrite(std::string &, std::vector<std::string> &, bool &); 
+void printOrWrite(std::string &, std::vector<std::string> &, bool &);
 std::tuple<uint8_t, uint8_t, uint8_t> uuidToColor(const std::string& );
 std::string rgbToAnsi(const std::tuple<uint8_t, uint8_t, uint8_t>& );
 
@@ -38,15 +38,15 @@ void waitForExit() { // wait until the user closes the programm by pressing ENTE
 
 void parseDeviceMetaData(Omniscope::MetaData metaData,
                          std::shared_ptr<OmniscopeDevice> &device) {
-  try {
-    nlohmann::json metaJson = nlohmann::json::parse(metaData.data);
-    fmt::println("{}", metaJson.dump());
-    device->setScale(std::stod(metaJson["scale"].dump()));
-    device->setOffset(std::stod(metaJson["offset"].dump()));
-    device->setEgu(metaJson["egu"]);
-  } catch (...) {
-    fmt::print("parsing Meta Data error: {}", metaData.data);
-  }
+    try {
+        nlohmann::json metaJson = nlohmann::json::parse(metaData.data);
+        fmt::println("{}", metaJson.dump());
+        device->setScale(std::stod(metaJson["scale"].dump()));
+        device->setOffset(std::stod(metaJson["offset"].dump()));
+        device->setEgu(metaJson["egu"]);
+    } catch (...) {
+        fmt::print("parsing Meta Data error: {}", metaData.data);
+    }
 }
 
 
@@ -56,86 +56,87 @@ void initDevices() { // Initalize the connected devices
 
     devices = deviceManager.getDevices(VID, PID);
     std::cout << "Found " << devices.size() << " devices.\n";
-    for(auto& device : devices){
+    for(auto& device : devices) {
         auto metaDataCb = [&](auto const &msg) {
             if (std::holds_alternative<Omniscope::MetaData>(msg)) {
                 parseDeviceMetaData(std::get<Omniscope::MetaData>(msg), device);
             }
-        }; 
+        };
         auto id = device->getId().value();
         auto sampleRate = static_cast<double>(id.sampleRate);
         device->setTimeScale(static_cast<double>(1 / sampleRate));
 
         auto [r, g, b] = uuidToColor(device->getId()->serial);
         device->send(Omniscope::SetRgb{static_cast<std::uint8_t>(static_cast<int>(r)),
-                                   static_cast<std::uint8_t>(static_cast<int>(g)),
-                                   static_cast<std::uint8_t>(static_cast<int>(b))});
-         // set Callback for MetaData
+                                       static_cast<std::uint8_t>(static_cast<int>(g)),
+                                       static_cast<std::uint8_t>(static_cast<int>(b))});
+        // set Callback for MetaData
         device->setMessageCallback(metaDataCb);
         device->send(Omniscope::GetMetaData{});
     }
 }
 
-void writeDatatoFile(std::map<Omniscope::Id, std::vector<std::pair<double, double>>> &captureData , std::string &filePath, std::vector<std::string> &UUID, bool &isJson) {
-    
-     if (captureData.empty()) {
+void writeDatatoFile(std::map<Omniscope::Id, std::vector<std::pair<double, double>>> &captureData, std::string &filePath, std::vector<std::string> &UUID, bool &isJson) {
+
+    if (captureData.empty()) {
         std::cerr << "No data available to write.\n";
         return;
     }
 
     // Datei öffnen
-    if(filePath.empty()){
-        std::string filePath = "data.txt"; 
+    if(filePath.empty()) {
+        std::string filePath = "data.txt";
     }
-    if(!isJson){
-    std::ofstream outFile(filePath, std::ios::app);
-    if (!outFile.is_open()) {
-        std::cerr << "Failed to open file: " << filePath << "\n";
-        return;
-    }
-
-    // Überprüfen, ob die Datei leer ist, um die Kopfzeile nur einmal zu schreiben
-    outFile.seekp(0, std::ios::end);
-    if (outFile.tellp() == 0) { // Datei ist leer
-        // Kopfzeile schreiben
-        outFile << "Timestamp";
-        for (const auto& id : UUID) {
-         outFile << id <<" , " ; 
+    if(!isJson) {
+        std::ofstream outFile(filePath, std::ios::app);
+        if (!outFile.is_open()) {
+            std::cerr << "Failed to open file: " << filePath << "\n";
+            return;
         }
-        outFile << "\n";
-    }
 
-    // Iteriere durch die erste ID, um die Reihenfolge der x-Werte zu bestimmen
-    auto firstDevice = captureData.begin();
-    const auto& firstDeviceData = firstDevice->second;
-
-    // Anzahl der Zeilen basierend auf der Größe des ersten Geräts
-    size_t numRows = firstDeviceData.size();
-
-    for (size_t i = 0; i < numRows; ++i) {
-        // Schreibe x-Wert und y-Wert der ersten ID
-        double xValue = firstDeviceData[i].first;
-        double yValueFirst = firstDeviceData[i].second;
-
-        outFile << fmt::format("{},{}", xValue, yValueFirst);
-
-        // Schreibe die y-Werte der restlichen IDs für denselben Index
-        for (auto it = std::next(captureData.begin()); it != captureData.end(); ++it) {
-            const auto& deviceData = it->second;
-
-            if (i < deviceData.size()) {
-                double yValue = deviceData[i].second;
-                outFile << fmt::format(",{}", yValue);
-            } else {
-                // Falls keine weiteren Werte für dieses Gerät vorhanden sind
-                outFile << ",N/A";
+        // Überprüfen, ob die Datei leer ist, um die Kopfzeile nur einmal zu schreiben
+        outFile.seekp(0, std::ios::end);
+        if (outFile.tellp() == 0) { // Datei ist leer
+            // Kopfzeile schreiben
+            outFile << "Timestamp";
+            for (const auto& id : UUID) {
+                outFile << id <<" , " ;
             }
+            outFile << "\n";
         }
-        outFile << "\n"; // Zeilenumbruch nach allen IDs
-    }
 
-    outFile.close(); // Datei schließen
-    fmt::print("Data successfully written to {}\n", filePath);
+        // Iteriere durch die erste ID, um die Reihenfolge der x-Werte zu bestimmen
+        auto firstDevice = captureData.begin();
+        const auto& firstDeviceData = firstDevice->second;
+
+        // Anzahl der Zeilen basierend auf der Größe des ersten Geräts
+        size_t numRows = firstDeviceData.size();
+
+        for (size_t i = 0; i < numRows; ++i) {
+            // Schreibe x-Wert und y-Wert der ersten ID
+            double xValue = firstDeviceData[i].first;
+            double yValueFirst = firstDeviceData[i].second;
+
+            outFile << fmt::format("{},{}", xValue, yValueFirst);
+
+            // Schreibe die y-Werte der restlichen IDs für denselben Index
+            for (auto it = std::next(captureData.begin()); it != captureData.end(); ++it) {
+                const auto& deviceData = it->second;
+
+                if (i < deviceData.size()) {
+                    double yValue = deviceData[i].second;
+                    outFile << fmt::format(",{}", yValue);
+                }
+                else {
+                    // Falls keine weiteren Werte für dieses Gerät vorhanden sind
+                    outFile << ",N/A";
+                }
+            }
+            outFile << "\n"; // Zeilenumbruch nach allen IDs
+        }
+
+        outFile.close(); // Datei schließen
+        fmt::print("Data successfully written to {}\n", filePath);
     }
     /*else {
          // JSON-Objekt erstellen
@@ -155,7 +156,7 @@ void writeDatatoFile(std::map<Omniscope::Id, std::vector<std::pair<double, doubl
             deviceData.push_back({{"timestamp", x}, {"value", y}});
         }
 
-        std::string ID = std::to_string(id.serial); 
+        std::string ID = std::to_string(id.serial);
         // Daten der ID hinzufügen
         jsonData[ID] = deviceData;
     }
@@ -186,7 +187,6 @@ void printDevices() {
             std::string deviceId = device -> getId()->serial;
             fmt::print("{}Device: {}\033[0m\n", rgbToAnsi(uuidToColor(deviceId)), deviceId);
         }
-        std::cout << "With -p, --play and the UUID you can start a measurement. With -f, --file you can choose a path with the filename to save the data to. Press Enter to stop the measurement." << std::endl;
         devices.clear();
         deviceManager.clearDevices();
         running = false;
@@ -194,7 +194,6 @@ void printDevices() {
 }
 
 void searchDevices() {
-    std::cout << "Geräte werden gesucht" << std::endl;
     if(!sampler.has_value()) {
         devices.clear();
         deviceManager.clearDevices();
@@ -212,7 +211,7 @@ void selectDevices(std::vector<std::string> &UUID) {
                     devices.begin(),
                     devices.end(),
             [&UUID](const std::shared_ptr<OmniscopeDevice>& device) {
-                    return std::find(UUID.begin(), UUID.end(), device->getId()->serial) == UUID.end();
+                return std::find(UUID.begin(), UUID.end(), device->getId()->serial) == UUID.end();
             }),
             devices.end()
             );
@@ -232,12 +231,12 @@ void printOrWrite(std::string &filePath, std::vector<std::string> &UUID, bool &i
     if(sampler.has_value()) { // write Data into file
         captureData.clear();
         sampler->copyOut(captureData);
-        if(filePath.empty()){
-        for(const auto& [id, vec] : captureData) {
-                std::cout << "filepath empty" << std::endl; 
+        if(filePath.empty()) {
+            for(const auto& [id, vec] : captureData) {
                 fmt::print("dev: {}\n", id);
+                std::cout << "Time[s]" << " , " <<"Voltage[V]" <<std::endl;
                 for(const auto& [first, second] : vec) {
-                    std::cout << "Time:" << first << "," << "Voltage" << second << std::endl;
+                    std::cout << first << " " << second << std::endl;
                     if(!running) {
                         break;
                     }
@@ -245,13 +244,13 @@ void printOrWrite(std::string &filePath, std::vector<std::string> &UUID, bool &i
                 if(!running) {
                     break;
                 }
-         }
-        }
-        else {
-                writeDatatoFile(captureData, filePath, UUID, isJson);
             }
         }
+        else {
+            writeDatatoFile(captureData, filePath, UUID, isJson);
+        }
     }
+}
 
 void startMeasurementAndWrite(std::vector<std::string> &UUID, std::string &filePath, bool &isJson) {
     while(running) {
@@ -259,13 +258,13 @@ void startMeasurementAndWrite(std::vector<std::string> &UUID, std::string &fileP
 
         selectDevices(UUID);  // select only chosen devices
 
-        printOrWrite(filePath, UUID, isJson); // print the data in the console or save it in the given filepath 
+        printOrWrite(filePath, UUID, isJson); // print the data in the console or save it in the given filepath
     }
 }
 
 std::tuple<uint8_t, uint8_t, uint8_t> uuidToColor(const std::string& uuid) {
     // std::hash anwenden
-    size_t hashValue = std::hash<std::string>{}(uuid);
+    size_t hashValue = std::hash<std::string> {}(uuid);
 
     // Die ersten 3 Bytes extrahieren
     uint8_t r = (hashValue & 0xFF);         // Erste 8 Bits
